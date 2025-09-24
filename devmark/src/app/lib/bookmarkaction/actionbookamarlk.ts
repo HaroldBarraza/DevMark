1e6be5b26dd0f0067bc4060fbd8d1292e5a2f879
HaroldBarraza <132396790+HaroldBarraza@users.noreply.github.com>
2025-09-24 17:39:00 -0500

    cambio de test a bookmark en actions

--- devmark/src/app/lib/bookmarkaction/actionbookamarlk.ts
+++ devmark/src/app/lib/bookmarkaction/actionbookamarlk.ts
@@ -10,17 +10,17 @@ export async function addbookmark(formData: FormData) {
     const link = formData.get('link') as string
 
     await sql`
-    INSERT INTO test (title, link)
+    INSERT INTO bookmark (title, link)
     VALUES (${title},${link})`
 }
 
 export async function updatebookmark(id:number, title:string, link:string) {
-    await sql `UPDATE test set title = ${title}, link = ${link} where id = ${id}`;
+    await sql `UPDATE bookmark set title = ${title}, link = ${link} where id = ${id}`;
 
     revalidatePath('/');
 }
 
 export async function deletebookmark(id:number) {
-    await sql `DELETE FROM test WHERE id =${id}`
+    await sql `DELETE FROM bookmark WHERE id =${id}`
     revalidatePath('/')
 }
